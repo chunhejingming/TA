eb4e4097f1f0783fcc6ffb86c4a85e5e22ca36ba
chunhejingming <72794555+chunhejingming@users.noreply.github.com>
2020-12-31 00:20:15 +0800

    Add files via upload

---
+++ dweight.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int x =1+1;
+	int y =3;
+	int z =4;
+	int v = x * y * z;
+	printf("s=%d\n",v);
+	printf("To c or not to c,that is a question.\n");
+	printf("1+1=%d\n",x);
+	return 0;
+}  
